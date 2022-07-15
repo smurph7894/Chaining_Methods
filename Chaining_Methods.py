9108f787a54e2bed59ae06e63c3e99e70e7553ba
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2022-07-15 16:44:59 -0700

    Initial commit

---
+++ Chaining_Methods.py
@@ -0,0 +1,42 @@
+class user:
+    def __init__(self, first_name, last_name, email, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.age = age
+        self.is_rewards_member = False
+        self.gold_card_points = 0 
+
+    def display_info(self):
+        print(self.first_name)
+        print(self.last_name)
+        print(self.email)
+        print(self.age)
+        print(self.is_rewards_member)
+        print(self.gold_card_points)
+        return self
+
+    def enroll(self):
+        if self.is_rewards_member == True:
+            print("User already a member.")
+        else:
+            self.is_rewards_member = True
+            self.gold_card_points = 200
+        return self
+
+    def spend_points(self, amount):
+        if amount <= self.gold_card_points:
+            self.gold_card_points = self.gold_card_points - amount  
+        else:
+            print(f"{self.first_name} does not have enough points for this.")
+        return self
+
+    
+sarah = user("Sarah", "Murphree", "murphs@dojo.com", 28)
+beth = user("Beth", "Human", "Hums@email.com", 103)
+roger = user("Roger", "Grig", "grig@email.com", 14)
+
+sarah.enroll().spend_points(50).display_info().enroll()
+beth.enroll().spend_points(80).display_info()
+roger.display_info().spend_points(40)
+
